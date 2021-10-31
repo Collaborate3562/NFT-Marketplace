79508059d9734a0676e681b5d08c4d81b6fb4bb2
Dragonwarrior1234 <hys0305060273@gmail.com>
2021-10-31 12:26:40 +0300

    test

--- rust/token-metadata/program/src/utils.rs
+++ rust/token-metadata/program/src/utils.rs
@@ -158,42 +158,42 @@ pub fn create_or_allocate_account_raw<'a>(
     size: usize,
     signer_seeds: &[&[u8]],
 ) -> ProgramResult {
-    // let rent = &Rent::from_account_info(rent_sysvar_info)?;
-    // msg!("--> Require balance: {}", rent.minimum_balance(size));
-    // msg!("--> Account balance: {}", new_account_info.lamports());
-    // let required_lamports = rent
-    //     .minimum_balance(size)
-    //     .max(1)
-    //     .saturating_sub(new_account_info.lamports());
+    let rent = &Rent::from_account_info(rent_sysvar_info)?;
+    msg!("--> Require balance: {}", rent.minimum_balance(size));
+    msg!("--> Account balance: {}", new_account_info.lamports());
+    let required_lamports = rent
+        .minimum_balance(size)
+        .max(1)
+        .saturating_sub(new_account_info.lamports());
     msg!("--> Get required lamports");
 
-    // if required_lamports > 0 {
-    //     msg!("--> Transfer {} lamports to the new account", required_lamports);
-    //     invoke(
-    //         &system_instruction::transfer(&payer_info.key, new_account_info.key, required_lamports),
-    //         &[
-    //             payer_info.clone(),
-    //             new_account_info.clone(),
-    //             system_program_info.clone(),
-    //         ],
-    //     )?;
-    // }
+    if required_lamports > 0 {
+        msg!("--> Transfer {} lamports to the new account", required_lamports);
+        invoke(
+            &system_instruction::transfer(&payer_info.key, new_account_info.key, required_lamports),
+            &[
+                payer_info.clone(),
+                new_account_info.clone(),
+                system_program_info.clone(),
+            ],
+        )?;
+    }
 
-    // let accounts = &[new_account_info.clone(), system_program_info.clone()];
+    let accounts = &[new_account_info.clone(), system_program_info.clone()];
 
-    // msg!("--> Allocate space for the account");
-    // invoke_signed(
-    //     &system_instruction::allocate(new_account_info.key, size.try_into().unwrap()),
-    //     accounts,
-    //     &[&signer_seeds],
-    // )?;
+    msg!("--> Allocate space for the account");
+    invoke_signed(
+        &system_instruction::allocate(new_account_info.key, size.try_into().unwrap()),
+        accounts,
+        &[&signer_seeds],
+    )?;
 
-    // msg!("--> Assign the account to the owning program");
-    // invoke_signed(
-    //     &system_instruction::assign(new_account_info.key, &program_id),
-    //     accounts,
-    //     &[&signer_seeds],
-    // )?;
+    msg!("--> Assign the account to the owning program");
+    invoke_signed(
+        &system_instruction::assign(new_account_info.key, &program_id),
+        accounts,
+        &[&signer_seeds],
+    )?;
 
     Ok(())
 }
@@ -835,7 +835,7 @@ pub fn process_create_metadata_accounts_logic(
     if *metadata_account_info.key != metadata_key {
         return Err(MetadataError::InvalidMetadataKey.into());
     }
-    
+    msg!("--->  Here!");
     create_or_allocate_account_raw(
         *program_id,
         metadata_account_info,
